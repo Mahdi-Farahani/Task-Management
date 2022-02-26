c62dbb5bc29b8fe3fd7dd7296f4e2f70d8c58114
Mahdi Farahani <mahdifrhn91@gmail.com>
2022-02-26 08:32:08 +0300

    NODE- add task model

---
+++ server/models/task.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema(
+	{
+		title: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		description: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		priority: {
+			type: String,
+			trim: true,
+			required: false,
+		},
+		status: {
+			type: String,
+			required: true,
+			default: 'todo',
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
